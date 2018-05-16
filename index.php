b749caa1c2b445c9cf13c712ee673640ba4c2674
halbeisent <dev@halbeisen.fr>
2018-05-16 12:05:39 +0200

    Exercice 5 Partie 1 PHP Terminé et Corrigé

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title></title>
+  </head>
+  <body>
+    <?php
+    $nothingVar = null;
+    echo $nothingVar;
+    $nothingVar = 'Bonjour';
+    echo $nothingVar;
+    ?>
+  </body>
+</html>
